f92ad5318d15098c0cb5e07f23c195f5dc6f209e
Amber Deokate <deokateamber@gmail.com>
2026-08-31 23:56:10 +0530

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-1.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        
+        for (int i = 0; i < n; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return f(preorder, 0, n - 1, inorder, 0, n - 1);
+        
+    }
+
+
+    TreeNode f(int[] preorder, int i, int j, int[] inorder, int x, int y) {
+        if (i > j) {
+            return null;
+        }
+        if (i == j) {
+            return new TreeNode(preorder[i]);
+        }
+        int idx = map.get(preorder[i]);
+        // for (int k = x; k <= y; k++) {
+        //     if (inorder[k] == mid) {
+        //         idx = k;
+        //     }
+        // }
+        int lenLeft = idx - x;
+        int lenRight = y - idx;
+        TreeNode res = new TreeNode(preorder[i]);
+        TreeNode l = f(preorder, i + 1, i + 1 + lenLeft - 1, inorder, x, idx - 1);
+        TreeNode r = f(preorder, j - lenRight + 1, j, inorder, idx + 1, y);
+        res.left = l;
+        res.right = r;
+        return res;
+
+    }
+
+
+
+
+}
